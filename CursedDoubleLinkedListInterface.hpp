71b203e63a9ee1604f377fa9b8237c074c264096
Handmade Octopus <handmade.octopus@gmail.com>
2023-04-14 13:27:07 +0100

    Modified access to make it less cursed (oh no) and bring quality of life stuff

--- CursedDoubleLinkedListInterface.hpp
+++ CursedDoubleLinkedListInterface.hpp
@@ -1,7 +1,8 @@
-
+#ifndef CURSED_DOUBLE_LINKED_LIST_INTERFACE_HPP
+#define CURSED_DOUBLE_LINKED_LIST_INTERFACE_HPP
 // CursedDoubleLinkedListInterface.hpp
 // Library is what it name is - it's a cursed linked list interface for C++
-// but I made it for Arduino just to mess with peoples heads.
+// but I made it for Arduino just to mess with more people heads.
 //
 // It works great if you want to create objects and have them accesible from anywhere
 // Good for stack allocation as well as heap or static allocation
@@ -9,40 +10,52 @@
 // Its supposed to be used as intefrace and templated.
 // It will return pointer to the T object on iteration so it's useful to remove boilerplate code
 // Enjoy and please note this is MIT licensed so I am not responsible - especially for damage while reading it
+//
+// This library is **evil**, you have been warned
 
+// Derive it and pass your class type into T
 template <typename T>
 class CDLLI {
- public:
-    // Head and tail are static, this way you can use it easily with T.head for iteration
-    static CDLLI<T>* head;
-    static CDLLI<T>* tail;
-    // We only need next but previous is useful for handling it faster
+    // _head and _tail are static, this way you can use it easily with T._head for iteration
+    static CDLLI<T>* _head;
+    static CDLLI<T>* _tail;
+    // We only need _next but _prev is useful for handling it faster
     // and accesing nearby nodes faster
-    CDLLI<T>* next;
-    CDLLI<T>* previous;
+    CDLLI<T>* _next;
+    CDLLI<T>* _prev;
+ public:
     
-    // Initialization - on construction object is automatically added to the linked list at the back
-    explicit CDLLI(bool toFront = false) : next(nullptr) {
+    
+    
+    // Initialization - on construction object is automatically added to the linked list
+    explicit CDLLI(bool toFront = false) {
         if(toFront) {
-            
+            _prev = nullptr;
+            if(_head) _head->_prev = this;
+            _next = _head;
+            _head = this;
         } else {
-            previous = tail;
-            if(!head) head = this;
-            if(tail) tail->next = this;
-            tail = this;
+            _prev = _tail;
+            if(!_head) _head = this;
+            if(_tail) _tail->_next = this;
+            _tail = this;
+            _next = nullptr;
         }
-        
-        std::cout << sizeof(*this) << std::endl;
     }
     
     virtual ~CDLLI() {
-        if(previous) {
-            previous->next = next;
-            if(next) next->previous = previous;
-            else tail = previous;
-        } else head = nullptr;
+        if(_prev) _prev->_next = _next;
+        if(_next) _next->_prev = _prev;
+        if(_tail == this) _tail = _prev;
+        if(_head == this) _head = _next;
     }
     
+    // NOTE: use object.T::next() if object is inherited by different lists
+    T* next() { return ((T*) _next); }
+    T* previous() { return ((T*) _prev); }
+
+    static T* _head() { return ((T*) _head); }
+    static T* _tail() { return ((T*) _tail); }
     
     class Iterator {
         CDLLI<T>* current;
@@ -51,7 +64,7 @@ class CDLLI {
         explicit Iterator(CDLLI<T>* CDLLI) : current(CDLLI) {}
 
         Iterator& operator++() {
-            current = current->next;
+            current = current->_next;
             return *this;
         }
 
@@ -65,7 +78,7 @@ class CDLLI {
     };
 
     Iterator begin() {
-        return Iterator(head);
+        return Iterator(_head);
     }
 
     Iterator end() {
@@ -74,6 +87,8 @@ class CDLLI {
 };
 
 template <typename T>
-CDLLI<T>* CDLLI<T>::head = nullptr;
+CDLLI<T>* CDLLI<T>::_head = nullptr;
 template <typename T>
-CDLLI<T>* CDLLI<T>::tail = nullptr;
+CDLLI<T>* CDLLI<T>::_tail = nullptr;
+
+#endif
